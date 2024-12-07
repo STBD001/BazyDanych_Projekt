5296550b132242db2ef70e210e64d52563fbf3d0
STBD001 <162894593+STBD001@users.noreply.github.com>
2024-12-07 17:14:09 +0100

    Update views.py

--- Backend/bazy/szkola_jazdy/views.py
+++ Backend/bazy/szkola_jazdy/views.py
@@ -1,4 +1,37 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_encode
+from .forms import RegistrationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.is_active = False  # Ustawiamy użytkownika na nieaktywny
+            user.save()
+
+            # Generowanie tokena aktywacji
+            token = default_token_generator.make_token(user)
+            uid = urlsafe_base64_encode(str(user.pk).encode())
+            mail_subject = 'Weryfikacja adresu e-mail'
+            message = render_to_string('account/activation_email.html', {
+                'user': user,
+                'domain': 'twoja-domena.com',
+                'uid': uid,
+                'token': token,
+            })
+            send_mail(mail_subject, message, 'no-reply@yourdomain.com', [user.email])
+
+            return redirect('szkola_jazdy:login')
+    else:
+        form = RegistrationForm()
+
+    return render(request, 'szkola_jazdy/register.html', {'form': form})
 
 def home(request):
-    return HttpResponse("Witaj na stronie głównej szkoły jazdy!")
+    return render(request, 'szkola_jazdy/home.html')
