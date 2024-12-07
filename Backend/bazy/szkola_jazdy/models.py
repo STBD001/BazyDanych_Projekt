2e1896cda39ed64f0dad4b4cfd2062b6c652d6ac
STBD001 <162894593+STBD001@users.noreply.github.com>
2024-12-07 13:08:07 +0100

    Update models.py

--- Backend/bazy/szkola_jazdy/models.py
+++ Backend/bazy/szkola_jazdy/models.py
@@ -1,3 +1,59 @@
 from django.db import models
 
-# Create your models here.
+
+class Sala(models.Model):
+    pojemność = models.IntegerField()
+    dostępność = models.BooleanField()
+
+    def __str__(self):
+        return f"Sala {self.id}: pojemność {self.pojemność}, dostępność {self.dostępność}"
+
+
+class Samochód(models.Model):
+    numer_rejestracyjny = models.CharField(max_length=20)
+    model = models.CharField(max_length=50)
+    rok_produkcji = models.CharField(max_length=4)
+    dostępność = models.BooleanField()
+
+    def __str__(self):
+        return f"Samochód {self.model} ({self.numer_rejestracyjny})"
+
+
+class Użytkownik(models.Model):
+    TYP_UŻYTKOWNIKA = [
+        ('kursant', 'Kursant'),
+        ('instruktor', 'Instruktor'),
+    ]
+
+    email = models.EmailField(unique=True)
+    nrTelefonu = models.CharField(max_length=15, null=True, blank=True)
+    imię = models.CharField(max_length=50)
+    nazwisko = models.CharField(max_length=50)
+    data_urodzenia = models.DateField(null=True, blank=True)
+    typ_użytkownika = models.CharField(max_length=10, choices=TYP_UŻYTKOWNIKA)
+    godziny_wyjeżdżone = models.IntegerField(default=0)
+    posiadane_lekcje = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.imię} {self.nazwisko} ({self.typ_użytkownika})"
+
+
+class Zajęcia(models.Model):
+    sala = models.ForeignKey(Sala, on_delete=models.SET_NULL, null=True, blank=True)
+    samochód = models.ForeignKey(Samochód, on_delete=models.SET_NULL, null=True, blank=True)
+    kursanci = models.IntegerField()
+    instruktor = models.ForeignKey(Użytkownik, on_delete=models.SET_NULL, null=True, blank=True,
+                                   related_name='instruktor')
+    godzina_rozpoczęcia = models.TimeField()
+    godzina_zakończenia = models.TimeField()
+
+    def __str__(self):
+        return f"Zajęcia {self.id} - {self.godzina_rozpoczęcia} - {self.godzina_zakończenia}"
+
+
+class TabelaPośrednicząca(models.Model):
+    użytkownik = models.ForeignKey(Użytkownik, on_delete=models.CASCADE)
+    zajęcia = models.ForeignKey(Zajęcia, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('użytkownik', 'zajęcia')
