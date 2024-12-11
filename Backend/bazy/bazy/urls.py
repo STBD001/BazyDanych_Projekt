f4ea3fcbdeb1dc49ebe5833e28d06294f24f0491
STBD001 <stefanwoj25@gmail.com>
2024-12-11 21:12:05 +0100

    Revert "Merge remote-tracking branch 'origin/main'"
    This reverts commit fdf6fef135dab5c282c6203b26debd85c6ad9aea, reversing
    changes made to 43e8da674b5d5934dd3c28586375266d2b8432f1.

--- Backend/bazy/bazy/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('register/', include('szkola_jazdy.urls')),
-    path('', include('szkola_jazdy.urls')),
-]
